2a4be22b4eeebd646335f414a8921780823a54bc
xingye-p <61429880+xingye-p@users.noreply.github.com>
2021-03-11 18:01:28 -0700

    Lab2 Timer&Interrupt (Mega2560)

---
+++ led.h
@@ -0,0 +1,19 @@
+// Student Name: Xingye Peng
+// Due Date: 2/18/2021
+// Assignment: Lab2
+//
+// Description: LED header file
+//
+
+
+
+#ifndef LED_H
+#define LED_H
+
+//initial LEDs,returns void & takes no parameters
+void initLED();
+
+// Turn on LEDS within one code
+void turnOnLEDwithChar(unsigned char num_Of_Type);
+
+#endif
---
+++ timer.h
@@ -0,0 +1,19 @@
+// Student Name: Xingye Peng
+// Due Date: 2/18/2021
+// Assignment: Lab2
+//
+//Header file of Timer
+//
+
+
+
+
+
+#ifndef TIMER_H
+#define TIMER_H
+
+void initTimer0();
+
+void delayMs(int delay);
+
+#endif
